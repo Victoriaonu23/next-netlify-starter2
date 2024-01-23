bd9aae8dbdb6b420569a2bee0f328859b14ba440
Victoriaonu23 <136612702+Victoriaonu23@users.noreply.github.com>
2024-01-23 19:51:46 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
         <Header title="Welcome to my app!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
-         <Link href="http://www.youtube.com/redirect?q=http%3A%2F%2Fnaijaspider.com&page=53">Bligt /Link>
+         <Link href="http://www.youtube.com/redirect?q=http%3A%2F%2Fnaijaspider.com&page=53">Bligt </Link>
         </p>
       </main>
 
